24a012af7075291be7aad898d4c06040f49393ff
Harshit Sinha <harshit.sinha@shopify.com>
2020-04-03 01:24:46 -0400

    Updates

--- main.js
+++ main.js
@@ -2,8 +2,14 @@ const scraper = require('./scraper');
 const dataLayer = require('./data_layer');
 const notify = require('./notify');
 
-module.exports = async function main() {
+async function main() {
     let data = await scraper();
     notify(data);
     await dataLayer.createDataSnapshot(data);
+}
+
+module.exports = main;
+
+if(require.main == module) {
+    main();
 }
